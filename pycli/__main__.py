381a44bb3189c9624a0ebd5d1fd7e285446e8044
Aljosha Friemann <a.friemann@automate.wtf>
2017-12-13 11:54:54 +0100

    main file

---
+++ pycli/__main__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+.. module: TODO
+    :platform: TODO
+    :synopsis: TODO
+
+.. moduleauthor:: Aljosha Friemann a.friemann@automate.wtf
+"""
+
+from pycli.cli import root
+
+root()
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4 fenc=utf-8
